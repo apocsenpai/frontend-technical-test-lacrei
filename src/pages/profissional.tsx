ae411b0ff5af357de588f9378ac0f1ee27412b03
apocsenpai <jonatassennas@ufrrj.br>
2023-07-15 01:41:25 -0300

    feat: add professional page content

--- src/pages/profissional.tsx
+++ src/pages/profissional.tsx
@@ -1,12 +1,29 @@
 import * as React from "react";
 import type { HeadFC } from "gatsby";
+
 import Layout from "../components/Layout";
+import {
+  ContentDescriptionWithBorder,
+  ContentTitle,
+  ContentWrapper,
+  Image,
+} from "../components/Layout/styled";
+import professionalImg from "../images/professional_img.svg";
 
 export default function Profissional() {
   return (
     <Layout>
-      <h1>Profissional</h1>
-      <p>I'm makin this by following the Gatsby Tutorial.</p>
+      <ContentWrapper>
+        <ContentTitle>Profissional</ContentTitle>
+        <ContentDescriptionWithBorder>
+          A Lacrei garante que pessoas LGBTQIAPN + recebam atendimento realizado
+          por profissionais de qualidade e que atendam às suas necessidades de
+          forma segura e acolhedora.
+        </ContentDescriptionWithBorder>
+      </ContentWrapper>
+      <ContentWrapper>
+        <Image src={professionalImg} alt="Profissional" />
+      </ContentWrapper>
     </Layout>
   );
 }
