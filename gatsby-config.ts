14baa1def3387238c0205218f4073281601df99c
apocsenpai <jonatassennas@ufrrj.br>
2023-07-14 22:23:56 -0300

    config: add new gatsby plugins

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,13 +3,37 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Frontend lacrei`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-styled-components"]
+  plugins: [
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.jpeg",
+      },
+    },
+    "gatsby-plugin-styled-components",
+    {
+      resolve: `gatsby-omni-font-loader`,
+      options: {
+        enableListener: true,
+        preconnect: [
+          `https://fonts.googleapis.com`,
+          `https://fonts.gstatic.com`,
+        ],
+        web: [
+          {
+            name: `Nunito`,
+            file: `https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap`,
+          },
+        ],
+      },
+    },
+  ],
 };
 
 export default config;
