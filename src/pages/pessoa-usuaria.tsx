4fec4f9471433099152e851029ca1b1a89378d14
apocsenpai <jonatassennas@ufrrj.br>
2023-07-15 01:41:13 -0300

    feat: add user page content

--- src/pages/pessoa-usuaria.tsx
+++ src/pages/pessoa-usuaria.tsx
@@ -1,14 +1,33 @@
 import * as React from "react";
 import type { HeadFC } from "gatsby";
+
 import Layout from "../components/Layout";
+import {
+  ContentDescriptionWithBorder,
+  ContentTitle,
+  ContentWrapper,
+  Image,
+} from "../components/Layout/styled";
+import userImg from "../images/user_img.svg";
 
 export default function PessoaUsuaria() {
   return (
     <Layout>
-      <h1>PessoaUsuaria</h1>
-      <p>I'm makin this by following the Gatsby Tutorial.</p>
+      <ContentWrapper>
+        <ContentTitle>Pessoa Usuária</ContentTitle>
+        <ContentDescriptionWithBorder>
+          A Lacrei garante que pessoas LGBTQIAPN + recebam atendimento realizado
+          por profissionais de qualidade e que atendam às suas necessidades de
+          forma segura e acolhedora.
+        </ContentDescriptionWithBorder>
+      </ContentWrapper>
+      <ContentWrapper>
+        <Image src={userImg} alt="User" />
+      </ContentWrapper>
     </Layout>
   );
 }
 
-export const Head: HeadFC = () => <title>Pessoa Usuária | Desafio Lacrei</title>;
+export const Head: HeadFC = () => (
+  <title>Pessoa Usuária | Desafio Lacrei</title>
+);
